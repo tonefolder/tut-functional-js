7d7f386847bedad8c06fd48faa43c1f1a7f24b30
unknown <mporecchadmin@SFODLITLOAD05.reedsmith.com>
2015-03-27 17:50:35 -0700

    'init_update'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var beerData = JSON.parse(document.getElementById("beerData").textContent);
+var beerData = new Beer();
 var beerTemplate = document.getElementById("tmpl-beer").textContent;
 var beerList = document.getElementById("beerList");
 var filters = document.getElementById("filters");
@@ -10,56 +10,65 @@ filters.addEventListener('click', function (e) {
   e.preventDefault();
   var beers = beerData.beers;
   var clicked = e.target;
-  var filter = clicked.dataset.filter;
-  var filteredBeers = [];
-  var i;
 
-  for (i=0; i<filterLinks.length; i++) {
+  if (clicked.dataset.hasOwnProperty('filter')) {
+    var filter = JSON.parse(clicked.dataset.filter);
+    var filteredBeers = filter === 'all' ? beers : filterBeers(beers, filter);
+
+    loadBeers(beerList, beerTemplate, filteredBeers);
+    setActiveFilter(clicked);
+  }
+});
+
+function filterBeers(data, query) {
+  var filteredData = [];
+  if (query === 'domestic' || query === 'import') {
+    for (i=0; i<data.length; i++) {
+      if (data[i].locale === query) {
+        filteredData.push(data[i]);
+      }
+    }
+  } else {
+    for (var i=0; i<data.length; i++) {
+      if (compareTypes(data[i], query)) {
+        filteredData.push(data[i]);
+      }
+    }
+  }
+  return filteredData;
+}
+
+function compareTypes(item, value) {
+  if (!Array.isArray(value)) {
+    return item.type === value;
+  }
+  for (var i=0; i<value.length; i++) {
+    if (item.type === value[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function makeFilter(property, callback) {
+  return function (value) {
+    return callback(property, value);
+  }
+}
+
+// var makeDescription = makeFilter('description', function (prop, value) {
+//   var retVal = {};
+//   retVal[prop] = value;
+//   return retVal;
+// });
+
+function loadBeers(list, template, data) {
+  list.innerHTML = _.template(template)({ beers: data });
+}
+
+function setActiveFilter(active) {
+  for (var i=0; i<filterLinks.length; i++) {
     filterLinks[i].classList.remove('btn-active');
   }
-
-  clicked.classList.add('btn-active');
-      
-  switch (filter) {
-    case 'all':
-      filteredBeers = beers;
-      break;
-    case 'domestic':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].locale === 'domestic') {
-          filteredBeers.push(beers[i]);
-        }
-      }
-      break;
-    case 'imports':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].locale === 'import') {
-          filteredBeers.push(beers[i]);
-        }
-      }
-      break;
-    case 'ale':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].type === 'ipa' || beers[i].type === 'ale') {
-          filteredBeers.push(beers[i]);
-        }
-      }
-      break;
-    case 'lager':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].type === 'lager') {
-          filteredBeers.push(beers[i]);
-        }
-      }
-      break;
-    case 'stout':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].type === 'stout') {
-          filteredBeers.push(beers[i]);
-        }
-      }
-      break;
-  }
-    
-  beerList.innerHTML = _.template(beerTemplate)({ beers: filteredBeers });
-});
+  active.classList.add('btn-active');
+}
---
+++ beer.js
@@ -0,0 +1,78 @@
+(function () {
+  window.Beer = function () {
+      this.beers = [
+          {
+            "name": "Bitter Hop",
+            "type": "ipa",
+            "locale": "domestic",
+            "abv": 7.9
+          },
+          {
+            "name": "Dragonblood Black IPA",
+            "type": "ipa",
+            "locale": "domestic",
+            "abv": 6.5
+          },
+          {
+            "name": "Good Dog Lager",
+            "type": "lager",
+            "locale": "domestic",
+            "abv": 4.8
+          },
+          {
+            "name": "Good Dog Snowpants",
+            "type": "stout",
+            "locale": "domestic",
+            "abv": 8.1
+          },
+          {
+            "name": "Lake Erie 2x IPA",
+            "type": "ipa",
+            "locale": "domestic",
+            "abv": 9.3
+          },
+          {
+            "name": "Lake Erie Session",
+            "type": "ale",
+            "locale": "domestic",
+            "abv": 4.2
+          },
+          {
+            "name": "Samwell British Pale Ale",
+            "type": "ale",
+            "locale": "import",
+            "abv": 6.5
+          },
+          {
+            "name": "Samwell Oatmeal Stout",
+            "type": "stout",
+            "locale": "import",
+            "abv": 5.5
+          },
+          {
+            "name": "Samwell Winter Ale",
+            "type": "ale",
+            "locale": "import",
+            "abv": 6.8
+          },
+          {
+            "name": "Sparkwood Beer Five",
+            "type": "lager",
+            "locale": "domestic",
+            "abv": 6.2
+          },
+          {
+            "name": "Sparkwood Campfire Lager",
+            "type": "lager",
+            "locale": "domestic",
+            "abv": 5.8
+          },
+          {
+            "name": "Tartan Scottish Ale",
+            "type": "ale",
+            "locale": "import",
+            "abv": 7.5
+          }
+      ];
+  };
+})();
